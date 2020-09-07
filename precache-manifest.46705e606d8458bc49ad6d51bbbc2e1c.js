0e7fdd6f13a6c39e954249f5872dbdafb6328de7
murryIsDeveloping <marcusbrincat1@gmail.com>
2020-09-07 20:33:06 +1000

    Updates

---
+++ precache-manifest.46705e606d8458bc49ad6d51bbbc2e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218108a5ed5f46b6754bdf4d6b48e805",
+    "url": "/2048-reaction/index.html"
+  },
+  {
+    "revision": "820b609099d7c2373c54",
+    "url": "/2048-reaction/static/css/main.82770a1b.chunk.css"
+  },
+  {
+    "revision": "fcf1cc2fce88cb445302",
+    "url": "/2048-reaction/static/js/2.4f2d3f8b.chunk.js"
+  },
+  {
+    "revision": "119c89aef8a0dbcac4acfcfac4cbdf48",
+    "url": "/2048-reaction/static/js/2.4f2d3f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820b609099d7c2373c54",
+    "url": "/2048-reaction/static/js/main.58d91335.chunk.js"
+  },
+  {
+    "revision": "60159c9f97baeab2b224",
+    "url": "/2048-reaction/static/js/runtime-main.341d15ed.js"
+  }
+]);
